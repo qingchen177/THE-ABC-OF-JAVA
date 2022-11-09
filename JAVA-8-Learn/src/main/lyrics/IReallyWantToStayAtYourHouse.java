eeb170941db37e17061a9f007544f89d73f9d3f7
ljl <ljl@chinaliancheng.com>
2022-11-09 16:40:11 +0800

    提交类型：歌词上传 提交人：company

---
+++ JAVA-8-Learn/src/main/lyrics/IReallyWantToStayAtYourHouse.java
@@ -0,0 +1,132 @@
+package main.lyrics;
+
+/**
+ * @author qingchen
+ * @date 9/10/2022 上午 9:23
+ */
+
+public class IReallyWantToStayAtYourHouse {
+    public static void main(String[] args) {
+        System.out.println(
+                "I couldn't wait for you to come clear the cupboards\n" +
+                "我无法等待你来清理这橱柜\n" +
+                "But now you're gone and leaving nothing but a sign\n" +
+                "但如今你离去 只留下一处标记\n" +
+                "Another evening I'll be sitting reading in between your lines\n" +
+                "又一个夜晚 我静坐品味你言外之意\n" +
+                "Because I miss you all the time\n" +
+                "但我却时刻思念着你\n" +
+                "\n" +
+                "So, get away\n" +
+                "所以 那就离开吧\n" +
+                "Another way to feel what you didn't want yourself to know\n" +
+                "只为让自己不愿了解真相\n" +
+                "And let yourself go\n" +
+                "放任你自己走吧\n" +
+                "You know you didn't lose your self-control\n" +
+                "你深知 你并未失去自制力\n" +
+                "Let's start at the rainbow\n" +
+                "让我们于彩虹之上开始\n" +
+                "Turn away\n" +
+                "拒绝内心纷扰\n" +
+                "Another way to be where you didn't want yourself to go\n" +
+                "只为让自己不愿继续前行\n" +
+                "Let yourself go\n" +
+                "放任你自己走吧\n" +
+                "Is that a compromise\n" +
+                "这是否等于妥协了呢\n" +
+                "\n" +
+                "So what do you wanna do, what's your point-of-view\n" +
+                "所以你想做什么呢 说出你的观点\n" +
+                "There's a party screaming do you wanna go\n" +
+                "这里有场狂欢派对 你是否想去呢\n" +
+                "A handshake with you, what's your point-of-view\n" +
+                "同你握手 询问你的看法\n" +
+                "I'm on top of you, I don't wanna go\n" +
+                "我全心倾覆于你 不愿离去\n" +
+                "'Cause I really wanna stay at your house\n" +
+                "因为我真的想要呆在你家里\n" +
+                "And I hope this works out\n" +
+                "\n" +
+                "But you know how much you broke me apart\n" +
+                "但你知道 你令我内心多么分崩离析\n" +
+                "I'm done with you, I'm ignoring you\n" +
+                "我已经受够你了 我要将你忽视\n" +
+                "I don't wanna know\n" +
+                "我不想再了解了\n" +
+                "\n" +
+                "And I'm aware that you were lying in the gutter\n" +
+                "我意识到 你深陷于恶劣环境中\n" +
+                "'Cause I did everything to be there by your side-ide\n" +
+                "因为我已竭尽全力 只为陪伴你身旁\n" +
+                "So when you tell me I'm the reason I just can't believe the lies\n" +
+                "所以当你告诉我 我就是其中的原因时 对于这些谎言 我简直难以置信\n" +
+                "And why do I so want to call you\n" +
+                "为何我这么想给你打电话\n" +
+                "\n" +
+                "So what do you wanna do, what's your point-of-view\n" +
+                "所以你想做什么呢 说出你的观点\n" +
+                "There's a party screaming do you wanna go\n" +
+                "这里有场狂欢派对 你是否想去呢\n" +
+                "A handshake with you, what's your point-of-view\n" +
+                "同你握手 询问你的看法\n" +
+                "I'm on top of you, I don't wanna go\n" +
+                "我全心倾覆于你 不愿离去\n" +
+                "'Cause I really wanna stay at your house\n" +
+                "因为我真的想要呆在你家里\n" +
+                "And I hope this works out\n" +
+                "\n" +
+                "But you know how much you broke me apart\n" +
+                "但你知道 你令我内心多么分崩离析\n" +
+                "I'm done with you, I'm ignoring you\n" +
+                "我已经受够你了 我要将你忽视\n" +
+                "I don't wanna know\n" +
+                "我不想再了解了\n" +
+                "\n" +
+                "You\n" +
+                "\n" +
+                "Oh-oh-oh\n" +
+                "\n" +
+                "I don't know why I'm no-one\n" +
+                "我不知为何 自己的内心犹如沉溺水下\n" +
+                "\n" +
+                "So, get away\n" +
+                "所以 那就离开吧\n" +
+                "Another way to feel what you didn't want yourself to know\n" +
+                "只为让自己不愿了解真相\n" +
+                "And let yourself go\n" +
+                "放任你自己走吧\n" +
+                "You know you didn't lose your self-control\n" +
+                "你深知 你并未失去自制力\n" +
+                "Let's start at the rainbow\n" +
+                "让我们于彩虹之上开始\n" +
+                "Turn away\n" +
+                "拒绝内心纷扰\n" +
+                "Another way to be where you didn't want yourself to go\n" +
+                "只为让自己不愿继续前行\n" +
+                "Let yourself go\n" +
+                "放任你自己走吧\n" +
+                "Is that a compromise\n" +
+                "这是否等于妥协了呢\n" +
+                "\n" +
+                "So what do you wanna do, what's your point-of-view\n" +
+                "所以你想做什么呢 说出你的观点\n" +
+                "There's a party screaming do you wanna go\n" +
+                "这里有场狂欢派对 你是否想去呢\n" +
+                "A handshake with you, what's your point-of-view\n" +
+                "同你握手 询问你的看法\n" +
+                "I'm on top of you, I don't wanna go\n" +
+                "我全心倾覆于你 不愿离去\n" +
+                "'Cause I really wanna stay at your house\n" +
+                "因为我真的想要呆在你家里\n" +
+                "And I hope this works out\n" +
+                "\n" +
+                "But you know how much you broke me apart\n" +
+                "但你知道 你令我内心多么分崩离析\n" +
+                "I'm done with you, I'm ignoring you\n" +
+                "我已经受够你了 我要将你忽视\n" +
+                "I don't wanna know\n" +
+                "我不想再了解了");
+    }
+}
+
