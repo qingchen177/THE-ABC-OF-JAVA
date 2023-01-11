c7d160adb287bbd79e4ef22a9a4e1cf980872764
ljl <ljl@chinaliancheng.com>
2023-01-11 13:37:07 +0800

    提交类型：集合删除问题，三体book上传 提交人：company

--- JAVA-8-Learn/src/main/java/Test.java
+++ JAVA-8-Learn/src/main/java/Test.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,17 +11,47 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        List<String> list = new ArrayList<String>();
+
+        //集合删除问题
+
+        //直接删会报异常
+        List<String> list = new ArrayList<>();
         list.add("1");
         list.add("2");
+        list.add("3");
+        list.add("4");
         for (String item : list) {
-            if ("1".equals(item)) {
+            if ("2".equals(item)) {
                 list.remove(item);
             }
         }
         System.out.println(list);
+        //使用迭代器删除才可以
+//        list = new ArrayList<String>();
+//        list.add("1");
+//        list.add("2");
+//        list.add("3");
+//        list.add("4");
+//        System.out.println(list + "before");
+//        Iterator<String> iterator = list.iterator();
+//        while (iterator.hasNext()) {
+//            String next = iterator.next();
+//            if (next.equals("2")) {
+//                iterator.remove();
+//
+//            }
+//        }
+//
+//        System.out.println(list + "after");
+//
 
-        list = new ArrayList<String>();
-        list.add("1");
+        String uuid= String.valueOf(System.currentTimeMillis());
+        System.out.println(uuid);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmssSSSS");
+        String number = sdf.format(System.currentTimeMillis());//202211041552160219
+        System.out.println("根据当前时间戳生成唯一："+number);
+//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+//        String now = sdf.format(new Date());
+//        System.out.println(now.substring(0,10)+"///");
     }
 }
