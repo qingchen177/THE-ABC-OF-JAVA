947a5552d2ad8b05dfebba51d62d635724c6cbe2
ljl <ljl@chinaliancheng.com>
2023-02-16 16:42:22 +0800

    提交类型：日常测试程序 提交人：company

---
+++ JAVA-8-Learn/src/main/test/DailyTest.java
@@ -0,0 +1,15 @@
+package main.test;
+
+import java.util.HashMap;
+
+/**
+ * @author qingchen
+ * @date 16/2/2023 下午 3:15
+ */
+
+public class DailyTest {
+    public static void main(String[] args) {
+        HashMap<String,Object> map = new HashMap<String,Object>();
+        System.out.println("map = " + map);
+    }
+}
