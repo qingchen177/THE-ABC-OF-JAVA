43d2fa7c871d449bed13bd55b9b92a3187d0c8d0
ljl <ljl@chinaliancheng.com>
2023-02-27 17:32:01 +0800

    提交类型：每日单词 提交人：company

---
+++ English-Learn/src/february/Day27.java
@@ -0,0 +1,38 @@
+package february;
+
+/**
+ * @author qingchen
+ * @date 27/2/2023 下午 5:10
+ */
+
+public enum Day27 {
+
+    dishonest(1,"不诚实的"),
+    indicate(2,"表明"),
+    considerable(3,"相当多的"),
+    compel(4,"强迫"),
+    chain(5,"连锁店"),
+    majority(6,"大多数"),
+    impart(7,"传授"),
+    calculation(8,"计算"),
+    incompetent(9,"不称职的"),
+    distribution(10,"分配"),
+    regime(11,"制度"),
+    consultation(12,"商议"),
+    exploit(13,"开发，开采"),
+    expectation(14,"期望"),
+    awkward(15,"尴尬的，棘手的，笨拙的"),
+    quantifiable(16,"可量化的"),
+    consistent(17,"一致的"),
+    sponsor(18,"赞助商"),
+    ;
+
+    private Integer code;
+    private String translation;
+
+    Day27(Integer code, String translation) {
+        this.code = code;
+        this.translation = translation;
+
+    }
+}
